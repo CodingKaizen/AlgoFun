6febb70733e435954e6335400dc65e178b93c8ae
VDUCHEW <vidiuchiu@gmail.com>
2017-12-17 21:37:52 -0800

    complete(): reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedNumbers = n.toString()
+    .split('').reverse().join('');
+  
+  return parseInt(reversedNumbers) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//   let reversedNumber;
+//
+//   if(Math.sign(n) === 1) {
+//     reversedNumber = parseInt(n.toString()
+//       .split('').reverse().join('').replace(/0/, ""));
+//   } else if(Math.sign(n) === -1) {
+//     reversedNumber = parseInt("-" + n.toString()
+//       .split('').reverse().join('').replace(/0/, ""));
+//   } else {
+//     return 0;
+//   }
+//
+//   return reversedNumber;
+// }
