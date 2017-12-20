af57363598f2449f14fc6b30f83c602bc75f1029
VDUCHEW <vidiuchiu@gmail.com>
2017-12-19 21:16:54 -0800

    complete(): anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const cleanStrA = mapObj(stringA);
+  const cleanStrB = mapObj(stringB);
+  
+  if(Object.keys(cleanStrA).length !== Object.keys(cleanStrB).length) {
+    return false;
+  }
+  
+  for(char in cleanStrA) {
+    if(cleanStrA[char] !== cleanStrB[char]) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+function mapObj(str) {
+  const cleanMap = {};
+  
+  for(char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    cleanMap[char] = cleanMap[char] +1 || 1;
+  }
+  
+  return cleanMap;
+};
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   let str1Map = {};
+//   let str2Map = {};
+//
+//   const strA = stringA.replace(/[^\w]/gi, "").toLowerCase();
+//   const strB = stringB.replace(/[^\w]/gi, "").toLowerCase();
+//
+//   for(string of strA) {
+//     str1Map[string] = str1Map[string] +1 || 1;
+//   }
+//
+//   for(string of strB) {
+//     str2Map[string] = str2Map[string] +1 || 1;
+//   }
+//
+//   if(strA.length === strB.length) {
+//     return JSON.stringify(str1Map).split('').sort().join() === JSON.stringify(str2Map).split('').sort().join();
+//   }
+//
+//   return false;
+// }
